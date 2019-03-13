0dd0c21b24dd87f6fd0fcba2d801fe9fa3b06a0c
Stefano Rainieri <stefano.rainieri@fazland.com>
2019-03-13 14:02:11 +0100

    Added shuffle method on ArrayUtils class. Provided ArrayUtils typings file.

---
+++ lib/Utils/ArrayUtils.d.ts
@@ -0,0 +1,28 @@
+declare class ArrayUtils {
+    /**
+     * Recursively converts all the keys to camel case.
+     */
+    static toCamelCaseKeys(input: { [key: string]: any }): { [key: string]: any };
+
+    /**
+     * Recursively converts all the keys to snake case.
+     */
+    static toSnakeCaseKeys(input: { [key: string]: any }): { [key: string]: any };
+
+    /**
+     * Sort an object by keys.
+     */
+    static ksort(input: { [key: string]: any }): { [key: string]: any };
+
+    /**
+     * Shuffle array elements
+     */
+    static shuffle(input: any[]): any[];
+
+    /**
+     * Computes the difference between two objects.
+     */
+    static computePatchObject(originalData: { [key: string]: any }, newData: { [key: string]: any }): [string, any][];
+}
+
+export = ArrayUtils;
