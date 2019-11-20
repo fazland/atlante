28fba959f6de172f9eedb68aa5cd6c68611a6d89
Alessandro Chitolina <alekitto@gmail.com>
2019-11-20 17:43:25 +0100

    add missing constructor definition in http exception

--- lib/Exception/HttpException.d.ts
+++ lib/Exception/HttpException.d.ts
@@ -1,5 +1,10 @@
 declare class HttpException extends Error {
     /**
+     * Constructor.
+     */
+    constructor(message?: string, response?: Response);
+
+    /**
      * Response received by the API.
      */
     readonly response: Response|undefined;
